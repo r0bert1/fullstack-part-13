48c973630219f4468446cea52bdb938a7a7ba675
Robert Pakkanen <rpakkane@gmail.com>
2025-10-18 11:36:50 +0300

    Add support for users

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const { router: blogsRouter, errorHandler: blogsErrorHandler } = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use(blogsErrorHandler)
+
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const start = async () => {
   await connectToDatabase()
